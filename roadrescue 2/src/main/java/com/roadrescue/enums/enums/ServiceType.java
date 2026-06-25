c92fb6ed6e42eca912e4b18727629c41ed74945b
navodidarshika31-cloud <navodidarshika31@gmail.com>
2026-06-25 10:28:19 -0700

    enums
    wgn darshika

---
+++ roadrescue 2/src/main/java/com/roadrescue/enums/enums/ServiceType.java
@@ -0,0 +1,14 @@
+package com.roadrescue.enums;
+
+public enum ServiceType {
+    FLAT_TIRE,
+    BATTERY_JUMP,
+    BATTERY_REPLACEMENT,
+    ENGINE_REPAIR,
+    FUEL_DELIVERY,
+    TOWING,
+    BRAKE_REPAIR,
+    LOCKOUT,
+    OIL_CHANGE,
+    GENERAL_REPAIR
+}
