078b389c70972ba450d103cb358321b40be4c2b4
ishanshalithabandara365-ctrl <ishanshalithabandara365@gmail.com>
2026-06-30 16:44:15 +0530

    Update Contact Garages

--- roadrescue 2/src/main/java/com/roadrescue/dto/ContactGarageDTO.java
+++ roadrescue 2/src/main/java/com/roadrescue/dto/ContactGarageDTO.java
@@ -1,12 +1,16 @@
 package com.roadrescue.dto;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 @Data
 public class ContactGarageDTO {
+
     @NotBlank(message = "Subject is required")
     @Size(max = 150, message = "Subject must be under 150 characters")
     private String subject;
+
     @NotBlank(message = "Message is required")
     @Size(max = 2000, message = "Message must be under 2000 characters")
     private String message;
